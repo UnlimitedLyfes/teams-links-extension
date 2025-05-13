e6a7b8e8d7890e3607bc62f8126702308ce005fc
UnlimitedLyfes <130376264+UnlimitedLyfes@users.noreply.github.com.>
2025-05-14 02:21:13 +0800

    progress

--- popup.js
+++ popup.js
@@ -1,5 +1,26 @@
-const fs = require('fs');
+getScheduleJson('schedule.json').then(schedule => {
+    console.log(schedule);
+    formatDaysToArray(schedule);
+    console.log(schedule);
 
-const schedjson = fs.readFileSync('schedule.json', 'utf8');
-const schedule = JSON.parse(schedjson)
+    const current = new Date();
+    const currentDay = current.getDay();
 
+    schedule["links"]
+
+})
+
+
+
+async function getScheduleJson(file){
+    const response = await fetch(chrome.runtime.getURL(file));
+    const schedule = await response.json();
+    return schedule;
+}
+
+function formatDaysToArray(schedule){
+    schedule["links"].forEach((item) => {
+        // Split the "days" string into an array of days
+        item["days"] = item["days"].split(/(?=[A-Z])/); // Use positive lookahead to split on uppercase letters without removing them
+    });
+}
