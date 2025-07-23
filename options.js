1d50141ef5769777db4c4ac4ccf20c1ae73b8cfe
UnlimitedLyfes <130376264+UnlimitedLyfes@users.noreply.github.com.>
2025-07-23 18:44:42 +0800

    Turns out you cant save to file, so im boutta rewrite it so that it gets from chromes storage katamad

--- options.js
+++ options.js
@@ -18,7 +18,7 @@ getScheduleJson('schedule.json').then(schedule => {
         mainDiv.querySelector('[name="timeEnd"]').setAttribute('value', subject["time"][1]);
 
         const boolDays = formatDaysToBooleanArray(subject["days"]);
-        const dayInputs = mainDiv.querySelectorAll('[name="day"]');
+        const dayInputs = mainDiv.querySelectorAll('[name^="day"]');
         boolDays.forEach((isOn, index) => {
             if(isOn){
                 dayInputs[index].checked = true;
@@ -37,9 +37,6 @@ addButton.addEventListener("click", () => {
     entries_section.appendChild(clone);
 })
 
-//removing options
-
-
 //saving autosave portion
 
 //importing json
@@ -50,21 +47,41 @@ addButton.addEventListener("click", () => {
 schedule_form.addEventListener('submit', (event) => {
     event.preventDefault(); // Stop the default form submission
     const formData = new FormData(schedule_form);
+    newFormData = formDataToObjectWithArrays(formData);;
     const newSched = [];
     
-    //
+    //Format day array with delimeter "," (the hidden type input in the html)
+    newFormData['day'] = formatDaysToJsonFriendly(newFormData['day'])
+
+    //input into newSched Array
+    newFormData['name'].forEach((name, index) => {
+        newSched.push({
+          name: name,
+          days: newFormData["day"][index],
+          time: `${newFormData["timeStart"][index]}-${newFormData["timeEnd"][index]}`,
+          link: newFormData["link"][index],
+        });
+    })
+
+    console.log(newSched);
+    
     
     const schedJSON = { links: newSched };
-    const jsonString = JSON.stringify(schedJSON);
-    console.log(jsonString);
+    //const jsonString = JSON.stringify(schedJSON);
+    chrome.storage.sync.set(schedJSON, () => {
+        console.log("saved");
+    })
 
+    chrome.storage.sync.get(["links"], result => {
+        console.log(result.links);
+    })
 })
 
 
 
 
 
-// - func
+// - functions -
 async function getScheduleJson(file){
     const response = await fetch(chrome.runtime.getURL(file));
     const schedule = await response.json();
@@ -86,13 +103,46 @@ function generateScheduleClone(){
     const clone = schedule_template.content.cloneNode(true);
     const mainDiv = clone.querySelector(".schedule_unit");
     mainDiv.id = `s_${entry_id}`;
-    entry_id++;
 
+    // //id the days
+    // const daysArray = clone.querySelectorAll('.day');
+    // daysArray.forEach(day => {
+    //     day.name = `day_${entry_id}`;
+    // })
+
+    // add the remove button's functionality
     const removeButton = clone.querySelector(".schedule_unit_x");
     removeButton.addEventListener("click", () => {
     console.log("remove clicked");
     removeButton.parentNode.remove();
     });
 
+    entry_id++;
     return clone;
+}
+
+function formDataToObjectWithArrays(formData) {
+  const obj = {};
+  for (const [key, value] of formData.entries()) {
+    if (obj[key]) {
+      obj[key].push(value);
+    } else {
+      obj[key] = [value];
+    }
+  }
+  return obj;
+}
+
+function formatDaysToJsonFriendly(daysArray){
+    const newDays = [""];
+    let index = 0;
+    daysArray.forEach((day) => {
+      if (day != ",") newDays[index] += day;
+      else {
+        index++;
+        newDays[index] = "";
+      }
+    });
+    newDays.pop();
+    return newDays;
 }
